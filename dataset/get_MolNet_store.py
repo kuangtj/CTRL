c0d4aa21a8426fe2a84fe32aea6e05a1749981e9
kuangtj <ktj.for.sni@gmail.com>
2026-03-31 11:10:30 +0800

    Update get_MolNet_store.py

--- dataset/get_MolNet_store.py
+++ dataset/get_MolNet_store.py
@@ -130,6 +130,7 @@ def get_dataset(task_id_want):
                         mol_tmp = Chem.MolFromSmiles(smiles_list[idx])
                         if mol_tmp.GetNumHeavyAtoms() > 200:
                             print(f"The mol is too large(>200): {smiles_list[idx]}")
+                            continue
                         mol_smiles = Chem.MolToSmiles(mol_tmp)
                         
                         data_item = smiles_preporcess(mol_smiles, f"./data_store/{task_id}_mol_sdf/mol_{mol_smiles.replace('/', '_')[:245]}.sdf")
